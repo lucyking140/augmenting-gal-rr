c9f56caa9f91588a97281e182479225c1e366c8a
Lucy King <lucyking140@gmail.com>
2025-04-16 09:38:35 +0200

    adding 11th reading response

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -23,7 +23,7 @@ import { useCounter } from '../hooks/useCounterContext';
 
 export default function Home(like_count: number) {
   
-  let mostRecent = 10 // current number of responses shown
+  let mostRecent = 11 // current number of responses shown
   
   let parsedResps = JSON.parse(JSON.stringify(resps)).responses  
   
